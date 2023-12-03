71d5f30b92cc9c955a5d0f2dccb4866d5bb6e3dc
Angel Junior Chacaliaza Talla <chacaliazatalla@gmail.com>
2023-12-03 16:11:50 -0500

    avance de la pregunta 4

--- gestionTienda/urls.py
+++ gestionTienda/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('tiendas/', views.tiendas, name='tiendas'),
     path('productos/', views.productos, name='productos'),
     path('detalle_tienda/<int:tienda_id>/', views.detalle_tienda, name='detalle_tienda'),
+    
 ]
--- gestionTienda/views.py
+++ gestionTienda/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
+from .models import Tienda, Producto
+from .forms import TiendaForm, ProductoForm
 
 def tiendas(request):
-    # Implementa la lógica para mostrar la lista de tiendas
-    return render(request, 'tiendas.html')
+    tiendas = Tienda.objects.all()
+    if request.method == 'POST':
+        # Lógica para agregar una nueva tienda
+        form = TiendaForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = TiendaForm()
+
+    return render(request, 'tiendas.html', {'tiendas': tiendas, 'form': form})
 
 def productos(request):
-    # Implementa la lógica para mostrar la lista de productos
-    return render(request, 'productos.html')
+    productos = Producto.objects.all()
+    if request.method == 'POST':
+        # Lógica para agregar un nuevo producto
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProductoForm()
+
+    return render(request, 'productos.html', {'productos': productos, 'form': form})
 
 def detalle_tienda(request, tienda_id):
     # Implementa la lógica para mostrar los detalles de una tienda
