ad45668beea3b459c559226628417c04c387c90e
Angel Junior Chacaliaza Talla <chacaliazatalla@gmail.com>
2023-12-03 15:46:46 -0500

    generando urls

---
+++ gestionTienda/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('tiendas/', views.tiendas, name='tiendas'),
+    path('productos/', views.productos, name='productos'),
+    path('detalle_tienda/<int:tienda_id>/', views.detalle_tienda, name='detalle_tienda'),
+]
