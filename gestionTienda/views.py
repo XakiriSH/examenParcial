7a1c6bb516795c1762f6c1d060f881cc2226e6c4
Angel Junior Chacaliaza Talla <chacaliazatalla@gmail.com>
2023-12-03 15:45:46 -0500

    Importando las vistas

--- gestionTienda/views.py
+++ gestionTienda/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def tiendas(request):
+    # Implementa la lógica para mostrar la lista de tiendas
+    return render(request, 'tiendas.html')
+
+def productos(request):
+    # Implementa la lógica para mostrar la lista de productos
+    return render(request, 'productos.html')
+
+def detalle_tienda(request, tienda_id):
+    # Implementa la lógica para mostrar los detalles de una tienda
+    return render(request, 'detalle_tienda.html')
