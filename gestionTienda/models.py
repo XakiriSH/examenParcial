5ab8273483e88ff80cd461cbb31ef1f41f0ae631
Angel Junior Chacaliaza Talla <chacaliazatalla@gmail.com>
2023-12-03 15:44:02 -0500

    Creando modelos

--- gestionTienda/models.py
+++ gestionTienda/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Tienda(models.Model):
+    direccion = models.CharField(max_length=255)
+    provincia = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    fecha_creacion = models.DateField()
+    telefono_contacto = models.CharField(max_length=20)
+
+class Producto(models.Model):
+    descripcion = models.CharField(max_length=255)
+    codigo = models.CharField(max_length=50)
+    precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.IntegerField()
+    tienda_relacionada = models.ForeignKey(Tienda, on_delete=models.SET_NULL, null=True, blank=True)
